3724a7b2085683c62b1d4890744991d0f6116ebb
unknown <ashiq.buet73@gmail.com>
2024-10-24 02:48:07 +0600

    query task done

--- PostgreSQL_Assignment_07.sql
+++ PostgreSQL_Assignment_07.sql
@@ -1,4 +1,4 @@
--- Active: 1729489816636@@127.0.0.1@5432@university_db
+-- Active: 1729604035783@@127.0.0.1@5432@university_db
 
 
 
@@ -17,6 +17,17 @@ CREATE TABLE students(
     backend_mark INT,
     status VARCHAR(50)
 );
+
+
+-- INSERT Many student data 
+INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status) VALUES
+(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
 
 
 SELECT * FROM students;
@@ -59,16 +70,9 @@ VALUES(1,1,1),(2,1,2),(3,2,1),(4,3,2);
 
 SELECT * FROM enrollment;
 
--- Query 1 Start --
 
--- INSERT Many student data 
-INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status) VALUES
-(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
-(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
-(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
-(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
-(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
-(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+-- Query 1 Start --
 
 
 -- insert a new student record 
@@ -86,20 +90,40 @@ INSERT into students VALUES(8,'Ashiqe',28,'ashiqe@gmail.com',50,49,NULL);
 -- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
 
 SELECT s.student_name
-FROM students s 
-JOIN enrollment e ON s.student_id = e.student_id
-JOIN courses c ON e.course_id = c.course_id
-WHERE c.course_name = 'Next.js'
+FROM students s
+    JOIN enrollment e ON s.student_id = e.student_id
+    JOIN courses c ON e.course_id = c.course_id
+    WHERE c.course_name ='Next.js';
 
 -- Query 2 End --
 
 
 -- Query 3 Start --
 
+-- Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'.
+
+UPDATE students
+SET status = 'Awarded'
+WHERE student_id=(
+    SELECT student_id
+    FROM students
+    ORDER BY (frontend_mark + backend_mark) DESC
+    LIMIT 1
+);
+
+SELECT * FROM students;
+
 -- Query 3 End --
 
 
 -- Query 4 Start --
+
+-- Delete all courses that have no students enrolled.
+
+DELETE FROM courses WHERE course_id NOT IN(
+    SELECT DISTINCT course_id
+    FROM enrollment
+);
 
 -- Query 4 End --
 
@@ -107,4 +131,37 @@ WHERE c.course_name = 'Next.js'
 
 -- Query 5 Start --
 
+-- Retrieve the names of students using a limit of 2, starting from the 3rd student.
+
+SELECT student_name
+FROM students
+LIMIT 2 OFFSET 2;
+
 -- Query 5 End --
+
+-- Query 6 Start --
+
+
+SELECT c.course_name, COUNT(e.student_id) AS students_enrolled
+FROM courses c
+LEFT JOIN enrollment e ON c.course_id = e.course_id
+GROUP BY c.course_name;
+
+-- Query 6 End --
+
+-- Query 7 Start --
+
+-- Calculate and display the average age of all students.
+SELECT avg(age) AS average_age
+FROM students; 
+
+-- Query 7 End --
+
+-- Query 8 Start --
+
+-- Retrieve the names of students whose email addresses contain 'example.com'.
+SELECT student_name
+FROM students
+WHERE email LIKE '%example.com%';
+
+-- Query 8 End --
